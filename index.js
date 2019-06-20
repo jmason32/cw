0b58e2cfffe9c5cd2c908d6af124efc6f6e21bcc
jmason32 <jordanmason32@gmail.com>
2019-06-20 17:22:31 -0400

    index added

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const app = express();
+const router = express.Router();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+router.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/html/index.html'));
+});
+
+
+app.use('/', router);
+
+app.use(express.static(path.join(__dirname, 'public')));
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'ejs')
+//app.get('/', (req, res) => res.render('pages/index'));
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
